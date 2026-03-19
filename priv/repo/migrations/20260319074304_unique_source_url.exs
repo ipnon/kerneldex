ac37c132cc92f89b4a46a07961a4c77fb8a58b7f
Ipnon <ipnon.noetics@gmail.com>
2026-03-19 15:54:10 +0800

    Fix migration: dedupe existing source_urls before adding unique index

--- priv/repo/migrations/20260319074304_unique_source_url.exs
+++ priv/repo/migrations/20260319074304_unique_source_url.exs
@@ -1,8 +1,21 @@
 defmodule Kerneldex.Repo.Migrations.UniqueSourceUrl do
   use Ecto.Migration
 
-  def change do
+  def up do
+    # Remove duplicate source_urls, keeping the oldest (lowest id)
+    execute """
+    DELETE FROM kernels
+    WHERE id NOT IN (
+      SELECT MIN(id) FROM kernels GROUP BY source_url
+    )
+    """
+
     drop unique_index(:kernels, [:file_name])
     create unique_index(:kernels, [:source_url])
+  end
+
+  def down do
+    drop unique_index(:kernels, [:source_url])
+    create unique_index(:kernels, [:file_name])
   end
 end
